258d3586df1fbfab434ec6d2f90dd1bbfdad98db
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-11-02 11:27:33 +0100

    Correction et ajout de commentaires

--- questions.js
+++ questions.js
@@ -1,32 +1,33 @@
-//new Array() -> crée un tableau
-//array.unshift("1","2") -> insert un pu plusieurs éléments dans le début du tableau et retourne le nombre d'éléments dans le tableau, pareil que push sauf qu'il ajoute les éléments à la fin du tableau
 var CreationTableauLangages = function () {
-var array = new Array();
-var add = array.unshift("Html","CSS","Java","PHP");
+var array = ["Html","CSS","Java","PHP"];
   return array;
 }
 
 var CreationTableauNombres = function () {
-  var array = new Array();
-  var add = array.unshift(0,1,2,3,4,5);
+  var array = [0,1,2,3,4,5];
     return array;
 }
 //langages.splice(2,1,"Javascript") -> permet de supprimer un élément, et même d'en rajoute un autre en le passant en 3ème paramètre
 var RemplacementElement = function (langages) {
 var element = langages.splice(2, 1, "Javascript");
   return langages;
+  /*Meilleur solution car plus simple et demande moins de ressource
+  langages[2] = 'Javascript';
+  return langages;*/
 }
-
+//push() -> ajoute les éléments à la fin du tableau et retourne les éléments du tableau
 var AjoutElementLangages = function (langages) {
 var push = langages.push("Ruby", "Python");
   return langages;
 }
+//new Array() -> crée un tableau
+//array.unshift("1","2") -> insert un pu plusieurs éléments dans le début du tableau et retourne les éléments du tableau
 //parseFloat() -> convertit une chaine en en valeur numérique
 var AjoutElementNombres = function (nombres) {
-var unshift = nombres.unshift(parseFloat("-2"), parseFloat("-1"));
+var unshift = nombres.unshift(-2, -1);
   return nombres;
 }
-//langages.shift() -> supprime un élément
+//langages.shift() -> supprime le 1er élément
 var SuppressionPremierElement = function (langages) {
   var langage = langages.shift()
   return langages;
@@ -36,14 +37,14 @@ var SuppressionDernierElement = function (langages) {
   var langage = langages.pop()
   return langages;
 }
-//reseaux_sociaux_chaine.split() ->
+//reseaux_sociaux_chaine.split(',') -> permet de séparer la chaine en élément distinct et en faire un tableau
 var ConversionChaineTableau = function (reseaux_sociaux_chaine) {
 var reseau = reseaux_sociaux_chaine.split(',');
   return reseau;
 }
-//langages.toString() -> convertis le tableau en chaine
+//langages.join() -> convertis le tableau en chaine
 var ConversionTableauChaine = function (langages) {
-  return langages.toString();
+  return langages.join();
 }
 //langages.sort() -> tri dans l'ordre alphabétique (ou numérique) les éléments
 var TriTableau = function (reseaux_sociaux) {
